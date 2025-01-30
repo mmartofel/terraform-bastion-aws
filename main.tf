18842317f138d719119fb7e036da89519f0cd6de
mmartofel <mmartofe@redhat.com>
2025-01-30 12:59:05 +0100

    align all naming to bastion

--- main.tf
+++ main.tf
@@ -5,64 +5,69 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example_vpc" {
+resource "aws_vpc" "bastion-vpc" {
   cidr_block = "10.0.0.0/16"
+  
   tags = {
-    Name = "ExampleVPC"
+    Name = "bastion-vpc"
   }
 }
 
 # Create an Internet Gateway (IGW)
-resource "aws_internet_gateway" "example_igw" {
-  vpc_id = aws_vpc.example_vpc.id
+resource "aws_internet_gateway" "bastion-igw" {
+  vpc_id = aws_vpc.bastion-vpc.id
 
   tags = {
-    Name = "ExampleIGW"
+    Name = "bastion-igw"
   }
 }
 
 # Create a Public Subnet
-resource "aws_subnet" "example_subnet" {
-  vpc_id                  = aws_vpc.example_vpc.id
+resource "aws_subnet" "bastion-subnet" {
+  vpc_id                  = aws_vpc.bastion-vpc.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true # Assign public IPs automatically
+  map_public_ip_on_launch = true                    # Assign public IPs automatically
   
   tags = {
-    Name = "ExampleSubnet"
+    Name = "bastion-subnet"
   }
 }
 
 # Create a Route Table for the Public Subnet
-resource "aws_route_table" "example_rt" {
-  vpc_id = aws_vpc.example_vpc.id
+resource "aws_route_table" "bastion-rt" {
+  vpc_id = aws_vpc.bastion-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example_igw.id # Route internet traffic to the IGW
+    gateway_id = aws_internet_gateway.bastion-igw.id       # Route internet traffic to the IGW
   }
 
   tags = {
-    Name = "ExampleRouteTable"
+    Name = "bastion-rt"
   }
 }
 
-# Associate the Route Table with the Subnet (Make it Public)
-resource "aws_route_table_association" "example_rta" {
-  subnet_id      = aws_subnet.example_subnet.id
-  route_table_id = aws_route_table.example_rt.id
+# Associate the Route Table with the Subnet (make it public)
+resource "aws_route_table_association" "bastion-rta" {
+  subnet_id      = aws_subnet.bastion-subnet.id
+  route_table_id = aws_route_table.bastion-rt.id
+
+  tags = {
+    Name = "bastion-rta"
+  }    
 }
 
-# Create a Security Group that Allows SSH
-resource "aws_security_group" "example_sg" {
-  name        = "example-security-group"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.example_vpc.id
+# Create a Security Group that allows SSH
+resource "aws_security_group" "bastion-sg" {
+  name        = "bastion-security-group"
+  description = "Allow SSH inbound traffic for bastion host"
+  vpc_id      = aws_vpc.bastion-vpc.id
   
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere (not recommended for production)
+    cidr_blocks = ["0.0.0.0/0"]                     # Allow SSH from anywhere (not recommended for production)
   }
   
   egress {
@@ -71,38 +76,54 @@ resource "aws_security_group" "example_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "bastion-sg"
+  }    
 }
 
 # Generate a new SSH key pair
-resource "tls_private_key" "example_key" {
+resource "tls_private_key" "bastion-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
+
+  tags = {
+    Name = "bastion-key"
+  }    
 }
 
 # Create AWS Key Pair using the generated public key
-resource "aws_key_pair" "example_key_pair" {
-  key_name   = "example-key"
-  public_key = tls_private_key.example_key.public_key_openssh
+resource "aws_key_pair" "bastion-key-pair" {
+  key_name   = "bastion-key"
+  public_key = tls_private_key.bastion-key.public_key_openssh
+
+  tags = {
+    Name = "bastion-key-pair"
+  }  
 }
 
 # Save the private key locally
 resource "local_file" "private_key_file" {
-  content  = tls_private_key.example_key.private_key_pem
+  content  = tls_private_key.bastion-key.private_key_pem
   file_permission = "0600"
-  filename = "example-key.pem"
+  filename = "bastion-key.pem"
+
+  tags = {
+    Name = "private_key_file"
+  }
 }
 
 # Create an EC2 instance in the public subnet
-resource "aws_instance" "example_instance" {
-  # ami                          = "ami-00d7be712d19c601f"               # al2023-ami-2023.6.20250123.4-kernel-6.1-x86_64 (eu-central-1 region)
-  ami                          = "ami-0ac4dfaf1c5c0cce9"               # al2023-ami-2023.6.20250123.4-kernel-6.1-x86_64 (us-east-1 region)
-  instance_type                = "t2.micro"              # Free tier eligible
-  subnet_id                    = aws_subnet.example_subnet.id
-  vpc_security_group_ids       = [aws_security_group.example_sg.id]
-  associate_public_ip_address  = true # Attach a public IP
-  key_name                     = aws_key_pair.example_key_pair.key_name # Attach the new key pair
+resource "aws_instance" "bastion-instance" {
+  # ami                          = "ami-00d7be712d19c601f"                # al2023-ami-2023.6.20250123.4-kernel-6.1-x86_64 (eu-central-1 region)
+  ami                          = "ami-0ac4dfaf1c5c0cce9"                # al2023-ami-2023.6.20250123.4-kernel-6.1-x86_64 (us-east-1 region)
+  instance_type                = "t2.micro"                             # Free tier eligible
+  subnet_id                    = aws_subnet.bastion-subnet.id           # Attach to subnet
+  vpc_security_group_ids       = [aws_security_group.bastion-sg.id]     # Assign security group
+  associate_public_ip_address  = true                                   # Attach a public IP
+  key_name                     = aws_key_pair.bastion-key-pair.key_name # Attach the new key pair
   
   tags = {
-    Name = "ExampleInstance"
+    Name = "bastion-instance"
   }
 }
