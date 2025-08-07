f34a5ed2eefd42c2d95635469306571af65d715c
mmartofel <mmartofe@redhat.com>
2025-08-07 19:06:10 +0200

    fix protocol for ingress

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_security_group" "bastion-sg" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]                     # Allow HTTP from anywhere (if we test anything like httpd)
   }
 
